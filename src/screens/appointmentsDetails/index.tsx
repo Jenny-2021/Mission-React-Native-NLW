5c8d5d08ce10f8b5a85c7c355b41473958f17844
Jennifer Pereira <jennifer.rf2018@gmail.com>
2022-03-04 16:15:28 -0300

    added share button icon

--- src/screens/appointmentsDetails/index.tsx
+++ src/screens/appointmentsDetails/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { View } from "react-native";
 
-import { Header } from '../../components/header';
+import { Fontisto } from '@expo/vector-icons'
+import { BorderlessButton } from "react-native-gesture-handler";
 import { Background } from '../../components/background';
+import { Header } from '../../components/header';
+
 import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
 
 export function AppointmentsDetails() {
 
@@ -11,6 +15,16 @@ return (
     <Background>
         <Header
         title="Detalhes"
+        action={
+            <BorderlessButton>
+                <Fontisto
+                name="share"
+                size={24}
+                color={theme.colors.primary}
+                />
+            </BorderlessButton>
+
+        }
         />
 
     </Background>
