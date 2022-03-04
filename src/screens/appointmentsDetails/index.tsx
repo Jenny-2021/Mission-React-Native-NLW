567c80630490fb88165ae6b0323582512054304d
Jennifer Pereira <jennifer.rf2018@gmail.com>
2022-03-04 17:08:39 -0300

    added Banner

--- src/screens/appointmentsDetails/index.tsx
+++ src/screens/appointmentsDetails/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
+
 
 import { Fontisto } from '@expo/vector-icons'
 import { BorderlessButton } from "react-native-gesture-handler";
 import { Background } from '../../components/background';
 import { Header } from '../../components/header';
 
-import { styles } from "./styles";
-import { theme } from "../../global/styles/theme";
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+import  BannerImg  from "../../assets/banner.png"
+
+
 
 export function AppointmentsDetails() {
 
@@ -23,9 +27,22 @@ return (
                 color={theme.colors.primary}
                 />
             </BorderlessButton>
-
         }
         />
+
+        <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+        >
+            <View style={styles.bannercontent}>
+                <Text style={styles.title}>
+                    Lendários
+                </Text>
+                <Text style={styles.subtitle}>
+                É hoje que vamos chegar ao challenger sem perder uma partida da md10
+                </Text>
+            </View>
+        </ImageBackground>
 
     </Background>
 );
--- src/screens/appointmentsDetails/styles.ts
+++ src/screens/appointmentsDetails/styles.ts
@@ -1,8 +1,28 @@
 import { StyleSheet } from "react-native";
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create ({
     container: {
-        flex:1,
+        flex: 1,
+    },
+    banner: {
+        width:'100%',
+        height: 234,
+    },
+    bannercontent: {
+        flex: 1,
+        justifyContent:'flex-end',
+        paddingHorizontal:24,
+        marginBottom: 30
+    },
+    title: {
+        fontFamily:theme.fonts.title700,
+        fontSize: 28,
+        color:theme.colors.heading
+    },
+    subtitle: {
+        fontFamily:theme.fonts.text400,
+        fontSize: 13,
+        color:theme.colors.heading
     },
 })
