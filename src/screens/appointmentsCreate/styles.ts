a7ddba247a21f9be76c8d10c73adec47a59a8f6f
Jennifer Pereira <jennifer.rf2018@gmail.com>
2022-03-21 16:36:42 -0300

    new component 'smallInput' Added

--- src/screens/appointmentsCreate/styles.ts
+++ src/screens/appointmentsCreate/styles.ts
@@ -11,6 +11,52 @@ export const styles = StyleSheet.create ({
         color:theme.colors.heading
     },
     form: {
+        marginHorizontal: 24,
+        marginTop: 32
+    },
+    select: {
+        width: '100%',
+        height: 68,
+        flexDirection:'row',
+        borderColor: theme.colors.secondary50,
+        borderWidth: 1,
+        borderRadius: 8,
+        alignItems:'center',
+        paddingRight: 25,
+        overflow: 'hidden'
+    },
+    image: {
+        width: 64,
+        height: 68,
+        backgroundColor:theme.colors.secondary50,
+        borderWidth: 1,
+        borderRadius: 8
+
+
+    },
+    selectBody: {
+        flex: 1,
+        alignItems:'center',
+        paddingRight: 25
+        
+
+    },
+    field: {
+        width: '100%',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginTop: 30
+    },
+    column: {
+        flexDirection:'row',
+        alignItems: 'center'
+
+    },
+    divider: {
+        marginRight: 4,
+        fontSize: 15,
+        fontFamily:theme.fonts.text500,
+        color:theme.colors.highlight
 
     },
 })
